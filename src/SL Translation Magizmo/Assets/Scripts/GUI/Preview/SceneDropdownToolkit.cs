df42dcaa8881e18c15f9dbcfa00e2b2a9fd4f684
DockFrankenstein <franciszekpyrc@gmail.com>
2024-02-24 11:33:03 +0100

    Ported topbar to ui toolkit

---
+++ src/SL Translation Magizmo/Assets/Scripts/GUI/Preview/SceneDropdownToolkit.cs
@@ -0,0 +1,169 @@
+﻿using Fab.UITKDropdown;
+using JetBrains.Annotations;
+using Project.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+namespace Project.GUI.Preview
+{
+    public class SceneDropdownToolkit : MonoBehaviour
+    {
+        [SerializeField] UIDocument document;
+        [SerializeField] PreviewSceneManager sceneManager;
+
+        PopupButton _popupButton;
+        Button _backButton;
+        ScrollView _scroll;
+
+        public string Path { get; private set; } = string.Empty;
+        Dictionary<Button, ItemData> ButtonData { get; set; } = new Dictionary<Button, ItemData>();
+
+        private void Reset()
+        {
+            document = GetComponent<UIDocument>();
+        }
+
+        private void Start()
+        {
+            var root = document.rootVisualElement;
+
+            _popupButton = root.Q<PopupButton>("scene-dropdown");
+            _backButton = root.Q<Button>("scene-dropdown-back");
+            _scroll = root.Q<ScrollView>("scene-dropdown-items");
+
+            _popupButton.ChangeOpenedState(false);
+
+            _backButton.clicked += () =>
+            {
+                var parts = Path.Split('/');
+
+                if (parts.Length > 0)
+                    parts = parts
+                        .SkipLast(1)
+                        .ToArray();
+
+                Path = string.Join('/', parts);
+                Reload();
+            };
+
+            Reload();
+        }
+
+        void Reload()
+        {
+            _popupButton.text = PUtility.GenerateDisplayName(sceneManager.CurrentScene.path.Split('/').Last());
+
+            while (_items.Count > 0)
+                RemoveItem(_items[0]);
+
+            _backButton.visible = !string.IsNullOrEmpty(Path);
+
+            var directories = sceneManager.scenes
+                .Select(x => x.path)
+                .Where(x => x.StartsWith(Path))
+                .Select(x => x.Substring(Path.Length, x.Length - Path.Length))
+                .Select(x => Path.Length == 0 ? x : x.Substring(1, x.Length - 1))
+                .Select(x =>
+                {
+                    var splits = x.Split('/');
+                    return new KeyValuePair<string, int>(splits.First(), splits.Length);
+                })
+                .GroupBy(x => x.Key)
+                .Select(x => x.First())
+                .ToList();
+
+            foreach (var path in directories)
+            {
+                var item = GetItem();
+                item.text = PUtility.GenerateDisplayName(path.Key);
+                ButtonData[item].path = path.Key;
+
+                if (path.Value > 1)
+                    item.Add(new VisualElement().WithClass("dir-mark"));
+            }
+        }
+
+        void ChangePath(string newPath)
+        {
+            var scenes = sceneManager.scenes
+                .Where(x => x.path.StartsWith(newPath));
+
+            var isDirection = true;
+
+            if (scenes.Count() == 1)
+            {
+                var singleScene = scenes.First();
+                var nextPath = singleScene.path.Substring(newPath.Length, singleScene.path.Length - newPath.Length);
+                isDirection &= nextPath.Contains('/');
+            }
+
+            //Button leads to another directory
+            if (isDirection)
+            {
+                Path = newPath;
+                Reload();
+                return;
+            }
+
+            var scene = scenes.First();
+            sceneManager.SelectScene(scene);
+            _popupButton.ChangeOpenedState(false);
+        }
+
+        #region Item Factory
+        List<Button> _items = new List<Button>();
+
+        public void RemoveItem(Button item)
+        {
+            item.ChangeDispaly(false);
+            _items.Remove(item);
+            _scroll.Remove(item);
+
+            if (ButtonData.ContainsKey(item))
+                ButtonData.Remove(item);
+        }
+
+        public Button GetItem()
+        {
+            var item = CreateItem();
+
+            _items.Add(item);
+            _scroll.Add(item);
+
+            if (!ButtonData.ContainsKey(item))
+                ButtonData.Add(item, new ItemData());
+
+            return item;
+        }
+
+        public Button CreateItem()
+        {
+            var item = new Button();
+            item.clicked += () =>
+            {
+                if (!ButtonData.TryGetValue(item, out var data)) return;
+
+                var newPath = string.IsNullOrEmpty(Path) ?
+                    data.path :
+                    $"{Path}/{data.path}";
+
+                ChangePath(newPath);
+                Reload();
+            };
+
+            return item;
+        }
+        #endregion
+
+        class ItemData
+        {
+            public string path;
+        }
+    }
+}
---
+++ src/SL Translation Magizmo/Assets/Scripts/Utility/UI/PopupButton.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using UnityEngine.UIElements;
+
+namespace Project.UI
+{
+    public class PopupButton : Button
+    {
+        public new class UxmlFactory : UxmlFactory<PopupButton, UxmlTraits> { }
+
+        public new class UxmlTraits : TextElement.UxmlTraits
+        {
+            UxmlBoolAttributeDescription _opened = new UxmlBoolAttributeDescription()
+            {
+                name = "Opened",
+            };
+
+            public override void Init(VisualElement ve, IUxmlAttributes bag, CreationContext cc)
+            {
+                base.Init(ve, bag, cc);
+                var btn = (PopupButton)ve;
+
+                bool isOpened = btn.Opened;
+                if (_opened.TryGetValueFromBag(bag, cc, ref isOpened) && isOpened != btn.Opened)
+                    btn.ChangeOpenedState(isOpened);
+            }
+        }
+
+        public VisualElement Root { get; set; }
+
+        VisualElement Arrow { get; set; }
+        VisualElement ContentContainer { get; set; }
+
+        public bool Opened { get; private set; }
+        public event Action<bool> OnOpenedOrClosed;
+
+        public override VisualElement contentContainer => ContentContainer;
+
+        public VisualElement Content =>
+            Children().FirstOrDefault();
+
+        public PopupButton()
+        {
+            ContentContainer = new VisualElement();
+            ContentContainer.style.position = Position.Absolute;
+            ContentContainer.style.left = 0f;
+            ContentContainer.style.right = 0f;
+            ContentContainer.style.bottom = 0f;
+            ContentContainer.style.alignItems = Align.Stretch;
+            ContentContainer.style.height = 0f;
+            contentContainer.style.marginBottom = 0f;
+            contentContainer.style.marginLeft = 0f;
+            contentContainer.style.marginRight = 0f;
+            contentContainer.style.marginTop = 0f;
+            ContentContainer.name = "popup-content-container";
+            ContentContainer.AddToClassList("popup-content-container");
+            ContentContainer.ChangeDispaly(false);
+
+            Arrow = new VisualElement();
+            Arrow.style.width = 25f;
+            Arrow.style.height = 25f;
+            Arrow.style.position = Position.Absolute;
+            Arrow.style.top = new StyleLength(StyleKeyword.Auto);
+            Arrow.style.bottom = new StyleLength(StyleKeyword.Auto);
+            Arrow.style.right = 4f;
+            Arrow.name = "popup-button-arrow";
+            Arrow.AddToClassList("popup-button-arrow");
+
+            hierarchy.Add(Arrow);
+            hierarchy.Add(ContentContainer);
+
+            style.overflow = Overflow.Visible;
+            
+            clicked += () =>
+            {
+                ChangeOpenedState(!Opened);
+            };
+        }
+
+        public void ChangeOpenedState(bool state)
+        {
+            Opened = state;
+            ContentContainer.ChangeDispaly(Opened);
+            OnOpenedOrClosed?.Invoke(state);
+        }
+    }
+}
