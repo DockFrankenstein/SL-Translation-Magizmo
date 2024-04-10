d4bff00b2e4a311332ef282ae1d40d0f41a24ca8
Dock Frankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2024-04-10 20:25:56 +0200

    Made menu scenes version update to the currently selected sl version

---
+++ src/SL Translation Magizmo/Assets/Scripts/GUI/Preview/VersionElement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using TMPro;
+
+namespace Project.GUI.Preview
+{
+    public class VersionElement : PreviewElement
+    {
+        [Label("Text")]
+        [SerializeField] TMP_Text text;
+
+        private void Reset()
+        {
+            text = GetComponent<TMP_Text>();
+        }
+
+        public override void Reload()
+        {
+            text.text = manager.CurrentVersion.version.ToString();
+        }
+    }
+}
