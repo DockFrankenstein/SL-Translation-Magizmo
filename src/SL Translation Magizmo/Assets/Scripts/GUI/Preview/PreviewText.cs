deda45f86c4e94b06bb67e9f852de4fa607bd5df
Dock Frankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2024-04-13 20:36:44 +0200

    Added weapon inventory scene

---
+++ src/SL Translation Magizmo/Assets/Scripts/GUI/Preview/PreviewText.cs
@@ -0,0 +1,30 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Project.GUI.Preview
+{
+    public class PreviewText : PreviewElement
+    {
+        [SerializeField] TMP_Text text;
+        [TextArea] public string[] contents;
+
+        private void Reset()
+        {
+            text = GetComponent<TMP_Text>();
+        }
+
+        protected override void Awake()
+        {
+            base.Awake();
+
+            if (text != null && contents.Length > 0)
+                text.text = contents[0];
+        }
+
+        public override void ChangeIndex(int newIndex, bool silent = false)
+        {
+            base.ChangeIndex(newIndex, silent);
+            text.text = contents[Mathf.Clamp(newIndex, 0, contents.Length - 1)];
+        }
+    }
+}
