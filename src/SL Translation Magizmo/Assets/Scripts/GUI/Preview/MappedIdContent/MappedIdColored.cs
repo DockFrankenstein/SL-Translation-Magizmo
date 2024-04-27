4277303a6a167b46f22667c132f7ad7f6512b163
Dock Frankenstein <49789311+DockFrankenstein@users.noreply.github.com>
2024-04-27 15:22:48 +0200

    Added an option to disable rich text for mapped id colored content

--- src/SL Translation Magizmo/Assets/Scripts/GUI/Preview/MappedIdContent/MappedIdColored.cs
+++ src/SL Translation Magizmo/Assets/Scripts/GUI/Preview/MappedIdContent/MappedIdColored.cs
@@ -1,17 +1,27 @@
 ﻿using System;
 using System.Linq;
 using UnityEngine;
-using static Project.GUI.Preview.MappedIdColored;
 
 namespace Project.GUI.Preview
 {
-    public class MappedIdColored : MappedIdContent<Context>
+    public class MappedIdColored : MappedIdContent<MappedIdColored.Context>
     {
+        public bool useRichText;
+
         public override string[] GetContentWithContext(GetContentArgs args, Context context) =>
             args.normalContent
             .Select(x => string.IsNullOrWhiteSpace(x) ? args.defaultContent : x)
-            .Select(x => $"<color=#{ColorUtility.ToHtmlStringRGBA(context.color)}>{x}</color>")
+            .Select(x => $"<color=#{ColorUtility.ToHtmlStringRGBA(context.color)}>{GetContent(x)}</color>")
             .ToArray();
+
+        string GetContent(string x)
+        {
+            if (useRichText)
+                return x;
+
+            return x
+                .Replace(">", "<space=0>>");
+        }
 
         [Serializable]
         public class Context
